019ef1313c67d8d03d5164f537901a9d0be2a7fc
sikarwar2010 <sikarwar2010@gmail.com>
2025-10-28 22:42:04 +0530

    feat:tailark

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { LoginLink } from "@kinde-oss/kinde-auth-nextjs";
+import { HeroHeader } from "@/components/header";
+import HeroSection from "@/components/hero-section";
 
 export default function Home() {
     return (
-        <div className="flex flex-col items-center justify-center">
-            <h1>Create Next App</h1>
-            <LoginLink>Login</LoginLink>
+        <div className="">
+            <HeroHeader />
+
+            <HeroSection />
         </div>
     );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,5 @@
-'use client'
+'use client';
+
 import Link from 'next/link'
 import { Logo } from '@/components/logo'
 import { Menu, X } from 'lucide-react'
