0085443a9fb30c66958907db1a1b6d48dd2e64f8
sikarwar2010 <sikarwar2010@gmail.com>
2025-10-29 01:39:20 +0530

    feat: kinde auth

--- components/header.tsx
+++ components/header.tsx
@@ -3,10 +3,11 @@
 import Link from "next/link";
 import { Logo } from "@/components/logo";
 import { Menu, X } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import React from "react";
 import { cn } from "@/lib/utils";
 import { ModeToggle } from "./ModeToggle";
+import { LoginLink, LogoutLink, RegisterLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 const menuItems = [
     { name: "Features", href: "#link" },
@@ -18,6 +19,8 @@ const menuItems = [
 export const HeroHeader = () => {
     const [menuState, setMenuState] = React.useState(false);
     const [isScrolled, setIsScrolled] = React.useState(false);
+    const { getUser, isLoading } = useKindeBrowserClient();
+    const user = getUser();
 
     React.useEffect(() => {
         const handleScroll = () => {
@@ -26,6 +29,7 @@ export const HeroHeader = () => {
         window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
     }, []);
+
     return (
         <header>
             <nav data-state={menuState && "active"} className="fixed z-20 w-full px-2">
@@ -81,24 +85,65 @@ export const HeroHeader = () => {
                                     ))}
                                 </ul>
                             </div>
-                            <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
-                                <ModeToggle />
-                                <Button asChild variant="outline" size="sm" className={cn(isScrolled && "lg:hidden")}>
-                                    <Link href="#">
-                                        <span>Login</span>
-                                    </Link>
-                                </Button>
-                                <Button asChild size="sm" className={cn(isScrolled && "lg:hidden")}>
-                                    <Link href="#">
-                                        <span>Sign Up</span>
-                                    </Link>
-                                </Button>
-                                <Button asChild size="sm" className={cn(isScrolled ? "lg:inline-flex" : "hidden")}>
-                                    <Link href="#">
-                                        <span>Get Started</span>
-                                    </Link>
-                                </Button>
-                            </div>
+
+                            {isLoading ? null : (
+                                <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
+                                    {user ? (
+                                        <>
+                                            <Link
+                                                className={buttonVariants({
+                                                    variant: "outline",
+                                                    size: "sm",
+                                                })}
+                                                href="/dashboard"
+                                            >
+                                                <span>Dashboard</span>
+                                            </Link>
+
+                                            <LogoutLink
+                                                className={buttonVariants({
+                                                    variant: "outline",
+                                                    size: "sm",
+                                                })}
+                                            >
+                                                <span>Logout</span>
+                                            </LogoutLink>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <ModeToggle />
+                                            <LoginLink
+                                                className={buttonVariants({
+                                                    variant: "outline",
+                                                    size: "sm",
+                                                    className: cn(isScrolled && "lg:hidden"),
+                                                })}
+                                            >
+                                                Login
+                                            </LoginLink>
+                                            <RegisterLink
+                                                className={buttonVariants({
+                                                    variant: "outline",
+                                                    size: "sm",
+                                                    className: cn(isScrolled && "lg:hidden"),
+                                                })}
+                                            >
+                                                SignUp
+                                            </RegisterLink>
+                                            <div className={cn(isScrolled ? "lg:inline-flex" : "hidden")}>
+                                                <RegisterLink
+                                                    className={buttonVariants({
+                                                        variant: "outline",
+                                                        size: "sm",
+                                                    })}
+                                                >
+                                                    Get Started
+                                                </RegisterLink>
+                                            </div>
+                                        </>
+                                    )}
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
